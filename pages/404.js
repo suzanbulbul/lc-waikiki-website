2b3bdde9b84040cb78cfb1eb49bccf388d38ee4a
Suzan Bülbül <suzanbulbl@gmail.com>
2023-11-07 16:14:22 +0300

    Dev: added error page #3

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import notFound from '../public/animations/not-found.json';
+
+
+const Custom404 = () => {
+  return (
+    <div className='not-found'>
+      <Lottie className='animation' animationData={notFound} />
+    </div>
+  );
+};
+
+export default Custom404;
+
+  
