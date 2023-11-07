b7b99bf01878b405cef17dfed7b43a85f19d38db
Suzan Bülbül <suzanbulbl@gmail.com>
2023-11-07 16:05:48 +0300

    Dev: added comingsoon page #2

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router'; 
 
 // Components
 import Header from './header';
 import Footer from './footer';
 
 const Layout = ({ children }) => {
+  const router = useRouter();
 
+  if (router.pathname === '/comingsoon' || router.pathname === '/404') {
+    return <main>{children}</main>;
+  }
+  
   return (
     <div>
       <Header />
