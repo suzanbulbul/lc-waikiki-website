aa4e2dd2da1bac6e038bd0478022b71867e690a2
Suzan Bülbül <suzanbulbl@gmail.com>
2023-11-09 17:25:08 +0300

    Dev: Activated the selected product on the card detail #19

--- components/cardDetail.jsx
+++ components/cardDetail.jsx
@@ -2,23 +2,40 @@
 import React, { useEffect, useState } from 'react';
 
 function CardDetail({data}) {
-
   const [selectedColor, setSelectedColor] = useState();
+  const [selectedButton, setSelectedButton] = useState(null);
 
   useEffect(() => {
     if( data ){
       const selectColor= data.color.filter(item => item.enable === true);
       setSelectedColor(selectColor[0])
+
     }
   }, [data])
 
+  const handleButtonClick = (size) => {
+    setSelectedButton(size);
+  };
 
   const handleColorChange = (color) => {
     setSelectedColor(color);
   };
+  
+  const hanleAddCart = () => {
+    const index = selectedColor.size.findIndex((size) => size.size === selectedButton);
+    
+    if (index !== -1 && selectedColor.size[index].piece > 0) {      
+      selectedColor.size[index].piece = selectedColor.size[index].piece - 1;
+      setSelectedButton(null);
 
-  
-  
+      console.log("Sepete eklendi");
+    } else {
+      console.log("Sepete eklenemedi", selectedColor);
+    }
+  }
+
+  console.log(selectedColor)
+
   return (
     <div className="card-detail">
       {data && selectedColor && (
@@ -57,13 +74,21 @@ function CardDetail({data}) {
                 <div className="d-flex justify-left">
                   {selectedColor.size.map((size) => (
                     //selected
-                    <button className="product-size-item mx-2" key={size.id}>
+                    <button
+                      disabled={size.piece == 0}
+                      className={`product-size-item mx-2 ${
+                        size.piece != 0  && selectedButton === size.size && "selected"
+                      } ${size.piece == 0 && "disabled"}`}
+                      onClick={() => handleButtonClick(size.size)}
+                      key={size.id}
+                    >
                       {size.size}
+                      {size.piece}
                     </button>
                   ))}
                 </div>
               </div>
-              <button className="primary-button">SEPETE EKLE</button>
+              <button disabled={!selectedButton} onClick={hanleAddCart} className="primary-button">SEPETE EKLE</button>
             </div>
             <div className="card-body">
               <div className="product-color">
@@ -74,8 +99,9 @@ function CardDetail({data}) {
                   {data.color.map((color) => (
                     <button
                       key={color.id}
-                      //selected
-                      className="product-color-item"
+                      className={`product-color-item ${
+                        selectedColor.color === color.color && "selected"
+                      }`}
                       checked={selectedColor.id === color.id}
                       onClick={() => handleColorChange(color)}
                     >
